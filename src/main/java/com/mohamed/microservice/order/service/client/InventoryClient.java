c705afbf0d085efa2fa66aab87adceb2cd19d7b9
mohamedmedhat <mmr973320@gmail.com>
2025-03-05 23:12:23 +0200

    make Kafka setup

--- src/main/java/com/mohamed/microservice/order/service/client/InventoryClient.java
+++ src/main/java/com/mohamed/microservice/order/service/client/InventoryClient.java
@@ -1,10 +1,19 @@
 package com.mohamed.microservice.order.service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+
 public interface InventoryClient {
 
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     @GetExchange("api/v1/inventory")
-    public boolean isInStock(@RequestParam String skuCode,@RequestParam Integer quantity);
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        return false;
+    }
 }
